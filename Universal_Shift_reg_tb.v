0a148a8ecf5b1ecbb99ed3756b245e8cd2b24297
Shivam <shivamkumar282628@gmail.com>
2026-07-02 19:34:20 +0530

    Create Universal_Shift_reg_tb.v

---
+++ Universal_Shift_reg_tb.v
@@ -0,0 +1,48 @@
+`timescale 1ns / 1ps
+
+module Universal_shift_reg_tb;
+
+    // Inputs
+    reg clk;
+    reg clear;
+    reg [2:0] S;
+    reg [3:0] I;
+
+    // Outputs
+    wire [3:0] O;
+
+    // Unit Under Test (UUT)
+    Universal_shift_reg uut (
+        .O(O), 
+        .clk(clk), 
+        .clear(clear), 
+        .S(S), 
+        .I(I)
+    );
+
+    always #50 clk = ~clk;
+
+    initial begin
+        $dumpfile("dump.vcd");
+        $dumpvars(0, Universal_shift_reg_tb);
+
+        clk = 0;       
+        clear = 1;
+        S = 3'b011;
+        I = 4'b1001;
+
+        #10  clear = 0;
+        #50  clear = 1;
+
+        #140 S = 3'b100; I = 4'b0000;
+        #100 S = 3'b111;
+        #100 S = 3'b110;
+        #100 S = 3'b101;
+        #100 S = 3'b010;
+        #100 S = 3'b001;
+        #100 S = 3'b000;
+
+        #100 $finish;
+    end
+
+endmodule
